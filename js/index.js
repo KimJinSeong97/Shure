15478ab0c6ba184d06fd61b780eab932b4121929
KimJinSeong97 <100419154+KimJinSeong97@users.noreply.github.com>
2022-07-27 13:19:58 +0900

    220727
    shure index js editing...

--- js/index.js
+++ js/index.js
@@ -1,3 +1,56 @@
+//nav mouseover
+document.querySelector('.mm1').addEventListener('mouseover', function () {
+    document.querySelector('.sb1').style.display = 'flex';
+    document.querySelector('.sb2').style.display = 'none';
+    document.querySelector('.sb3').style.display = 'none';
+    document.querySelector('.sb4').style.display = 'none';
+    document.querySelector('#dark').classList.add('dark_mode');
+})
+document.querySelector('.mm2').addEventListener('mouseover', function () {
+    document.querySelector('.sb2').style.display = 'flex';
+    document.querySelector('.sb1').style.display = 'none';
+    document.querySelector('.sb3').style.display = 'none';
+    document.querySelector('.sb4').style.display = 'none';
+    document.querySelector('#dark').classList.add('dark_mode');
+})
+document.querySelector('.mm3').addEventListener('mouseover', function () {
+    document.querySelector('.sb3').style.display = 'flex';
+    document.querySelector('.sb1').style.display = 'none';
+    document.querySelector('.sb2').style.display = 'none';
+    document.querySelector('.sb4').style.display = 'none';
+    document.querySelector('#dark').classList.add('dark_mode');
+})
+document.querySelector('.mm4').addEventListener('mouseover', function () {
+    document.querySelector('.sb4').style.display = 'flex';
+    document.querySelector('.sb1').style.display = 'none';
+    document.querySelector('.sb2').style.display = 'none';
+    document.querySelector('.sb3').style.display = 'none';
+    document.querySelector('#dark').classList.add('dark_mode');
+})
+document.querySelector('.mm5').addEventListener('mouseover', function () {
+    document.querySelector('.sb4').style.display = 'none';
+    document.querySelector('.sb1').style.display = 'none';
+    document.querySelector('.sb2').style.display = 'none';
+    document.querySelector('.sb3').style.display = 'none';
+    document.querySelector('#dark').classList.add('dark_mode');
+})
+document.querySelector('header').addEventListener('mouseleave', function () {
+    document.querySelector('.sb1').style.display = 'none';
+    document.querySelector('.sb2').style.display = 'none';
+    document.querySelector('.sb3').style.display = 'none';
+    document.querySelector('.sb4').style.display = 'none';
+    document.querySelector('#dark').classList.remove('dark_mode');
+})
+
+//검색 버튼 클릭창
+$(function () {
+    $('#search_button').click(function () {
+        $('.search').slideToggle(250);
+        $('#dark').toggleClass('dark_mode');
+    })
+})
+
+
 //slide
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
@@ -12,8 +65,8 @@ next.onclick = function () {
         i = 0;
     } else {
         i++;
-    };
-};
+    }
+}
 
 document.querySelector('.btn1').addEventListener('click', function () {
     document.querySelector('.slide_box').style.transform = 'translate(0vw)';
@@ -32,22 +85,25 @@ document.querySelector('.btn4').addEventListener('click', function () {
 });
 
 // best seller slide
-$(function () {
-    var page = 0;
-    $('.prev2').click(function () {
-        page--;
-        if (page < 0) {
-            page = 0;
-            return;
-        }
-        $('.bs_box').stop().animate({ marginLeft: -400 * page }, 250)
-    })
-    $('.next2').click(function () {
-        page++;
-        if (page > $('.bs_box .bs').length - 3) {
-            page = $('.bs_box .bs').length - 3;
-            return;
-        }
-        $('.bs_box').stop().animate({ marginLeft: -400 * page }, 250)
-    })
-})
+const prev2 = document.querySelector('.prev2');
+const next2 = document.querySelector('.next2');
+const bsbox = document.querySelector('.bs_box');
+var page = 0;
+
+prev2.onclick = function () {
+    page--;
+    if (page < 0) {
+        page = 0;
+        return;
+    }
+    bsbox.style.marginLeft = -400 * page + 'px';
+};
+
+next2.onclick = function () {
+    page++;
+    if (page > document.querySelectorAll('.bs').length - 3) {
+        page = document.querySelectorAll('.bs').length - 3;
+        return;
+    }
+    bsbox.style.marginLeft = `${-400 * page}px`;
+};
