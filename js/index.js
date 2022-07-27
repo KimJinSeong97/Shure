92a2cb88e402a5d3b69f1ed00d8edcd00b2aa750
KimJinSeong97 <100419154+KimJinSeong97@users.noreply.github.com>
2022-07-27 18:29:26 +0900

    220727
    index js, html editing..!

--- js/index.js
+++ js/index.js
@@ -4,6 +4,11 @@ document.querySelector('.mm1').addEventListener('mouseover', function () {
     document.querySelector('.sb2').style.display = 'none';
     document.querySelector('.sb3').style.display = 'none';
     document.querySelector('.sb4').style.display = 'none';
+    this.classList.add('mm_font');
+    document.querySelector('.mm2').classList.remove('mm_font');
+    document.querySelector('.mm3').classList.remove('mm_font');
+    document.querySelector('.mm4').classList.remove('mm_font');
+    document.querySelector('.mm5').classList.remove('mm_font');
     document.querySelector('#dark').classList.add('dark_mode');
 })
 document.querySelector('.mm2').addEventListener('mouseover', function () {
@@ -11,6 +16,11 @@ document.querySelector('.mm2').addEventListener('mouseover', function () {
     document.querySelector('.sb1').style.display = 'none';
     document.querySelector('.sb3').style.display = 'none';
     document.querySelector('.sb4').style.display = 'none';
+    this.classList.add('mm_font');
+    document.querySelector('.mm1').classList.remove('mm_font');
+    document.querySelector('.mm3').classList.remove('mm_font');
+    document.querySelector('.mm4').classList.remove('mm_font');
+    document.querySelector('.mm5').classList.remove('mm_font');
     document.querySelector('#dark').classList.add('dark_mode');
 })
 document.querySelector('.mm3').addEventListener('mouseover', function () {
@@ -18,6 +28,11 @@ document.querySelector('.mm3').addEventListener('mouseover', function () {
     document.querySelector('.sb1').style.display = 'none';
     document.querySelector('.sb2').style.display = 'none';
     document.querySelector('.sb4').style.display = 'none';
+    this.classList.add('mm_font');
+    document.querySelector('.mm1').classList.remove('mm_font');
+    document.querySelector('.mm2').classList.remove('mm_font');
+    document.querySelector('.mm4').classList.remove('mm_font');
+    document.querySelector('.mm5').classList.remove('mm_font');
     document.querySelector('#dark').classList.add('dark_mode');
 })
 document.querySelector('.mm4').addEventListener('mouseover', function () {
@@ -25,6 +40,11 @@ document.querySelector('.mm4').addEventListener('mouseover', function () {
     document.querySelector('.sb1').style.display = 'none';
     document.querySelector('.sb2').style.display = 'none';
     document.querySelector('.sb3').style.display = 'none';
+    this.classList.add('mm_font');
+    document.querySelector('.mm1').classList.remove('mm_font');
+    document.querySelector('.mm2').classList.remove('mm_font');
+    document.querySelector('.mm3').classList.remove('mm_font');
+    document.querySelector('.mm5').classList.remove('mm_font');
     document.querySelector('#dark').classList.add('dark_mode');
 })
 document.querySelector('.mm5').addEventListener('mouseover', function () {
@@ -32,6 +52,11 @@ document.querySelector('.mm5').addEventListener('mouseover', function () {
     document.querySelector('.sb1').style.display = 'none';
     document.querySelector('.sb2').style.display = 'none';
     document.querySelector('.sb3').style.display = 'none';
+    this.classList.add('mm_font');
+    document.querySelector('.mm1').classList.remove('mm_font');
+    document.querySelector('.mm2').classList.remove('mm_font');
+    document.querySelector('.mm3').classList.remove('mm_font');
+    document.querySelector('.mm4').classList.remove('mm_font');
     document.querySelector('#dark').classList.add('dark_mode');
 })
 document.querySelector('header').addEventListener('mouseleave', function () {
@@ -39,33 +64,47 @@ document.querySelector('header').addEventListener('mouseleave', function () {
     document.querySelector('.sb2').style.display = 'none';
     document.querySelector('.sb3').style.display = 'none';
     document.querySelector('.sb4').style.display = 'none';
+    document.querySelector('.mm1').classList.remove('mm_font');
+    document.querySelector('.mm2').classList.remove('mm_font');
+    document.querySelector('.mm3').classList.remove('mm_font');
+    document.querySelector('.mm4').classList.remove('mm_font');
+    document.querySelector('.mm5').classList.remove('mm_font');
     document.querySelector('#dark').classList.remove('dark_mode');
 })
 
 //검색 버튼 클릭창
 $(function () {
-    $('#search_button').click(function () {
+    $('#search_button').on('click', function () {
         $('.search').slideToggle(250);
         $('#dark').toggleClass('dark_mode');
-    })
+    });
+    $('nav ul').mouseover(function () {
+        $('.search').css('display', 'none');
+    });
 })
-
 
 //slide
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
-const s_box = document.querySelector('.slide_box');
-var s_width = 100;
-var s_index = document.querySelectorAll('.slide_box > div');
+const sbox = document.querySelector('.slide_box');
 var i = 0;
 
 next.onclick = function () {
-    s_box.style.marginLeft = -s_width * i + 'vw';
-    if (i == s_index.length - 1) {
+    sbox.style.marginLeft = -100 * i + 'vw';
+    if (i == document.querySelectorAll('.slide_box > div').length - 1) {
         i = 0;
     } else {
         i++;
     }
+}
+
+next.onclick = function () {
+    i++;
+    if (i > document.querySelectorAll('.slide_box>div').length - 1) {
+        i = document.querySelectorAll('.slide_box>div').length - 1;
+        return;
+    }
+    sbox.style.marginLeft = -100 * i + 'vw';
 }
 
 document.querySelector('.btn1').addEventListener('click', function () {
